eed87a19251d0524e5d3891ed7f17c062fc6ec97
lukmam <112078810+Zeron91@users.noreply.github.com>
2022-08-29 10:13:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-/**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288213049469
-   * Follow https://github.com/Zeron91/bot-war
-*/
+
 
 const fs = require('fs')
 const chalk = require('chalk')
@@ -11,7 +7,7 @@ const chalk = require('chalk')
 global.owner = ['6288213049469']
 global.premium = ['6288213049469']
 global.ownername = 'Lukman XD'
-global.packname = 'Lukman XD??'
+global.packname = 'Lukman XD?'
 global.author = 'WhatsApp 👿'
 global.sessionName = 'KONYOL'
 global.linkgrupss = 'https://chat.whatsapp.com/GKNbQLxByhe2eZtEnZwF3Y'
@@ -36,95 +32,7 @@ global.thumb = fs.readFileSync('./lib/thum.jpg')
 global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='<p align="center">
-<img src="https://github.com/Zeron91/bot-war" alt="LUKMAN XD "width="200"/>
-
-<p align="center">
-    <a href="https://github.com/Zeron91/bot-war">
-        <img
-            src="https://readme-typing-svg.herokuapp.com?size=15&width=280&lines=Thank+for+using+lukman+bot+🤗"
-            alt="lukmanxD"
-        />
-    </a>
-</p>
-<p align="center">
-<a href="https://github.com/Zeron91/bot-war"><img title="Followers" src="https://img.shields.io/github/followers/zeeoneofc?color=red&style=flat-square"></a>
-<a href="https://github.com/Zeron91/bot-war"><img title="Stars" src="https://img.shields.io/github/stars/zeeoneofc/Haruka?color=blue&style=flat-square"></a>
-<a href="https://github.com/Zeron91/bot-war"><img title="Forks" src="https://img.shields.io/github/forks/zeeoneofc/Haruka?color=red&style=flat-square"></a>
-<a href="https://github.com/Zeron91/bot-war"><img title="Watching" src="https://img.shields.io/github/watchers/zeeoneofc/Haruka?label=Watchers&color=blue&style=flat-square"></a>
-<a href="https://github.com/Zeron91/bot-war"><img title="Open Source" src="https://badges.frapsoft.com/os/v2/open-source.svg?v=103"></a>
-<a href="https://github.com/Zeron91/bot-war"><img title="Size" src="https://img.shields.io/github/repo-size/zeeoneofc/Haruka?style=flat-square&color=green"></a>
-<a href="https://github.com/Zeron91/bot-war"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fzeeoneofc%2FHaruka&count_bg=%2379C83D&title_bg=%23555555&icon=probot.svg&icon_color=%2300FF6D&title=hits&edge_flat=false"/></a>
-<a href="https://github.com/Zeron91/bot-war"><img height="20" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg"></a>&nbsp;&nbsp;
-</p>
-
-</p>
-</div>
-
-
-# Instalasi
-## Heroku Buildpack
-
-Click the deploy icon below !
-
-[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/zeeoneofc/Haruka)
-
-```bash
- > heroku/nodejs
- > https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
- > https://github.com/clhuang/heroku-buildpack-webp-binaries.git
-```
-
-## Termux
-```bash
-> apt update && apt upgrade
-> pkg install libweb nodejs git ffmpeg
-> git clone https://github.com/Zeron91/bot-war
-> cd bot-war
-> npm install
-> node npm.start
-```
-
-## settings
-You can edit owner and other in `'./settings/config.json'`
-
-```ts
-{
-	global.owner = ['6288213049469']
-global.premium = ['6288213049469']
-global.ownername = 'Lukman XD'
-global.packname = 'Lukman XD??'
-global.author = 'WhatsApp 👿'
-global.sessionName = 'KONYOL'
-global.linkgrupss = ' https://chat.whatsapp.com/GKNbQLxByhe2eZtEnZwF3Y'
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
-global.mess = {
-    success: 'Donee',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Espere a que su solicitud sea procesada',
-    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
-}
-global.limitawal = {
-    premium: "Infinity",
-    free: 100
-}
-```
-## Donate
-BUAT BELI PC RANKIT BANH🗿
-(NOMER) (6288292024190)
-
-# Official Group
-- - [Group 5](https://chat.whatsapp.com/GKNbQLxByhe2eZtEnZwF3Y)
-
-# Thanks To
-- My Good
--Lukman Bot
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
